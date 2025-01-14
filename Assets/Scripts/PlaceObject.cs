c989ed5883541235fb31f8cb543498b9fb75eb37
Noah Vaters <144738466+NoV8rs@users.noreply.github.com>
2025-01-14 12:37:46 -0400

    Scaling Object
    Scaling now works with using two figures to scale the object up and down

--- Assets/Scripts/PlaceObject.cs
+++ Assets/Scripts/PlaceObject.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
@@ -8,65 +9,87 @@ using EnhancedTouchSupport = UnityEngine.InputSystem.EnhancedTouch;
 [RequireComponent(typeof(ARRaycastManager), typeof(ARPlaneManager))]
 public class PlaceObject : MonoBehaviour
 {
-    [SerializeField] public Slider scaleSlider;
-    [SerializeField] public Slider rotateSlider;
-    
     [SerializeField] private GameObject objectToPlace;
-    private int scaleObject;
-    private int rotateObject;
-    
+    private GameObject spawnedObject;
     private ARRaycastManager arRaycastManager;
     private ARPlaneManager arPlaneManager;
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
-    
+    private float initialDistance;
+    private Vector3 initialScale;
+
     private void Awake()
     {
         arRaycastManager = GetComponent<ARRaycastManager>();
         arPlaneManager = GetComponent<ARPlaneManager>();
     }
-    
+
     private void OnEnable()
     {
         EnhancedTouchSupport.EnhancedTouchSupport.Enable();
         EnhancedTouchSupport.TouchSimulation.Enable();
         EnhancedTouchSupport.Touch.onFingerDown += OnFigureDown;
+        EnhancedTouchSupport.Touch.onFingerMove += OnFigureMoveObject;
+        EnhancedTouchSupport.Touch.onFingerUp += OnFigureUp;
     }
-    
+
     private void OnDisable()
     {
         EnhancedTouchSupport.EnhancedTouchSupport.Disable();
         EnhancedTouchSupport.TouchSimulation.Disable();
-        
-        scaleSlider.onValueChanged.RemoveListener(delegate { ScaleObject(); });
-        rotateSlider.onValueChanged.RemoveListener(delegate { RotateObject(); });
+        EnhancedTouchSupport.Touch.onFingerDown -= OnFigureDown;
+        EnhancedTouchSupport.Touch.onFingerMove -= OnFigureMoveObject;
+        EnhancedTouchSupport.Touch.onFingerUp -= OnFigureUp;
     }
-    
+
     private void OnFigureDown(EnhancedTouchSupport.Finger finger)
     {
         if (finger.index != 0) return;
-        
+
         if (arRaycastManager.Raycast(finger.currentTouch.screenPosition, hits, TrackableType.PlaneWithinPolygon))
         {
-            foreach (var hit in hits)
+            var hitPose = hits[0].pose;
+            spawnedObject = Instantiate(objectToPlace, hitPose.position, hitPose.rotation);
+        }
+    }
+
+    private void OnFigureMoveObject(EnhancedTouchSupport.Finger finger)
+    {
+        if (spawnedObject == null) return;
+
+        if (EnhancedTouchSupport.Touch.activeFingers.Count == 1)
+        {
+            if (arRaycastManager.Raycast(finger.currentTouch.screenPosition, hits, TrackableType.PlaneWithinPolygon))
             {
-                var hitPose = hit.pose;
-                Instantiate(objectToPlace, hitPose.position, hitPose.rotation);
+                var hitPose = hits[0].pose;
+                spawnedObject.transform.position = hitPose.position;
             }
         }
-        
-        scaleSlider.onValueChanged.AddListener(delegate { ScaleObject(); });
-        rotateSlider.onValueChanged.AddListener(delegate { RotateObject(); });
+        else if (EnhancedTouchSupport.Touch.activeFingers.Count == 2)
+        {
+            var finger1 = EnhancedTouchSupport.Touch.activeFingers[0];
+            var finger2 = EnhancedTouchSupport.Touch.activeFingers[1];
+
+            if (finger1.currentTouch.phase == UnityEngine.InputSystem.TouchPhase.Began || finger2.currentTouch.phase == UnityEngine.InputSystem.TouchPhase.Began)
+            {
+                initialDistance = Vector2.Distance(finger1.screenPosition, finger2.screenPosition);
+                initialScale = spawnedObject.transform.localScale;
+            }
+            else
+            {
+                float currentDistance = Vector2.Distance(finger1.screenPosition, finger2.screenPosition);
+                if (Mathf.Approximately(initialDistance, 0)) return;
+
+                float scaleFactor = currentDistance / initialDistance;
+                spawnedObject.transform.localScale = initialScale * scaleFactor;
+            }
+        }
     }
     
-    private void ScaleObject()
+    private void OnFigureUp(EnhancedTouchSupport.Finger finger)
     {
-        scaleObject = (int) scaleSlider.value;
-        objectToPlace.transform.localScale = new Vector3(scaleObject, scaleObject, scaleObject);
-    }
-    
-    private void RotateObject()
-    {
-        rotateObject = (int) rotateSlider.value;
-        objectToPlace.transform.rotation = Quaternion.Euler(0, rotateObject, 0);
+        if (EnhancedTouchSupport.Touch.activeFingers.Count < 2)
+        {
+            initialDistance = 0;
+        }
     }
 }
